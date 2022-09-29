17bd4b701c922122b7902dd1fed816c1ee9eff64
jonatasandradedeveloper <jonatasandrade.develop@gmail.com>
2022-09-28 22:52:43 -0300

    First commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Logo from './assets/logo.png';
+
+import { StyleSheet, Text, TextInput, TouchableOpacity, Image, View } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Image source={Logo} style={styles.logo} />
+
+      <TextInput
+          style={styles.input}  
+          placeholder="Digite seu e-mail"
+          placeholderTextColor="#3C3C3D"/>
+      
+
+      <TextInput
+        style={styles.input}  
+        placeholder="Digite sua senha"
+        placeholderTextColor="#3C3C3D"
+        secureTextEntry />
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.entry}>Entrar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,4 +36,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 150,
+    height: 150,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#646464',
+    padding: 10,
+    height: 50,
+    width: '90%',
+    marginTop: 20,
+    borderRadius: 10,
+
+  },
+  button: {
+    backgroundColor: '#EA4335',
+    width: '40%',
+    marginTop: 50,
+    padding: 12,
+    borderRadius: 10,
+  },
+  entry:{
+    color: '#FFF',
+    textAlign: 'center',
+  }
 });
---
+++ Readme.md
@@ -0,0 +1,12 @@
+<H1 style="margin-bottom: 50px">Tela de Login</H1>
+
+<img src="./assets/login.png" >
+
+<ol>
+<li>Jonatas Andrade da Silva RA: 2201911</li>
+<li>Lucas Martins Lima RA: 2201426</li>
+<li>Pricilla Souza RA: 2200555</li>
+<li>William Santos Nunes RA: 2200274</li>
+<li>Clarindo de Sal RA: 2200105</li>
+<li>Daniel Ferreira Valle RA:1600204</li>
+</ol>
